17526d0e70830a63dc540b9569155e50f9f1ce81
Carlarys <260849525+Carlarys@users.noreply.github.com>
2026-04-15 19:17:14 +0200

    Now it's kind of a real app

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
 "use strict";
-
-const data = [
-  ["13:00", 18, "☁️", 0.0],
-  ["14:00", 17, "🌧️", 0.4],
-  ["15:00", 17, "☁️", 0.1],
-  ["16:00", 16, "🌦️", 0.3],
-  ["17:00", 15, "☁️", 0.0],
-  ["18:00", 14, "🌥️", 0.0]
-];
-
-const forecast = document.querySelector("#forecast");
-
-for (const item of data) {
-  const row = document.createElement("div");
-  row.className = "row";
-  row.innerHTML = `
-        <div class="time">${item[0]}</div>
-        <div class="temp">${item[1]} °C</div>
-        <div class="icon" aria-label="Bewölkung">${item[2]}</div>
-        <div class="rain">${item[3].toFixed(1)} mm</div>
-      `;
-  forecast.append(row);
-}
-
-/**
-      * Läd die Wetterdaten für die angegebenen Koordinaten.
-      * Koordinaten werden berechnet via https://epsg.io/4326
-      * OpenAPI-Doku für den Mode forecast: https://dataset.api.hub.geosphere.at/v1/openapi-docs#/forecast/Timeseries_Forecast_timeseries_forecast__resource_id__get
-      * @param {string} Welche Wetter-Parameter das API liefern soll.
-      */
-
 function getCoordinates() {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -37,6 +6,13 @@ function getCoordinates() {
 }
 
 async function getweather(parameter, coordinates) {
+  /**
+   * Läd die Wetterdaten für die angegebenen Koordinaten.
+   * Koordinaten werden berechnet via https://epsg.io/4326
+   * OpenAPI-Doku für den Mode forecast: https://dataset.api.hub.geosphere.at/v1/openapi-docs#/forecast/Timeseries_Forecast_timeseries_forecast__resource_id__get
+   * @param {string} Welche Wetter-Parameter das API liefern soll.
+   */
+
   const url = `https://dataset.api.hub.geosphere.at/v1/timeseries/forecast/nwp-v1-1h-2500m?parameters=${parameter}&lat_lon=${coordinates}`;
 
   const res = await fetch(url);
@@ -45,30 +21,49 @@ async function getweather(parameter, coordinates) {
   }
   const json = await res.json();
 
-  const now = new Date().getHours();
-  const offset = 2;
-
+  // get forcast from JSON
   const forecast = json.features[0].properties.parameters[parameter].data;
 
-  const units = {
-    t2m: "°C",
-    rr_acc: "mm",
-  };
-  const unit = units[parameter] ?? "";
+  return forecast;
+}
 
-  for (let i = offset; i < 8; i++) {
-    console.log(`${(now + i) % 24} → ${forecast[i].toFixed(0)} ${unit}<br>`);
+function printtoscreen(t2m_forecast, rr_acc_forecast) {
+  // set timezone
+  const now = new Date();
+  const hours = new Date().getHours();
+  const timezone = Math.abs(now.getTimezoneOffset() / 60);
+  console.log(`Timezone offset: ${timezone}`);
+
+  const grid = document.querySelector("#forecast");
+
+  for (let i = timezone; i < 8; i++) {
+    console.log(`${(hours + i) % 24} → ${t2m_forecast[i].toFixed(0)}`);
+    console.log(`${(hours + i) % 24} → ${rr_acc_forecast[i].toFixed(0)}`);
+
+    const row = document.createElement("div");
+    // FIXME: hier gehören noch die wforecast-Elemente rein
+    row.className = "row";
+    row.innerHTML = `
+        <div class="time">${(hours + i) % 24}h</div>
+        <div class="temp">${t2m_forecast[i].toFixed(0)} °C</div>
+        <div class="icon" aria-label="Bewölkung">--</div>
+        <div class="rain">${rr_acc_forecast[i].toFixed(0)} mm</div>
+      `;
+    grid.append(row);
   }
 }
 
 async function main() {
+
   const pos = await getCoordinates();
   const coordinates = `${pos.coords.latitude},${pos.coords.longitude}`;
 
   console.log(`Koordinaten: ${coordinates}`);
 
-  await getweather("t2m", coordinates);
-  await getweather("rr_acc", coordinates);
+  let t2m_forecast = await getweather("t2m", coordinates);
+  let rr_acc_forecast = await getweather("rr_acc", coordinates);
+
+  printtoscreen(t2m_forecast, rr_acc_forecast);
 }
 
 main();
